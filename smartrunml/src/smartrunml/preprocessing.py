d3c6b2ee07e07d954d64c7a422c8f4f09a674333
Yian <yi-an.shih@rwth-aachen.de>
2025-06-22 19:25:21 +0200

    simple train model second version

--- smartrunml/src/smartrunml/preprocessing.py
+++ smartrunml/src/smartrunml/preprocessing.py
@@ -3,20 +3,32 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
+def convert_pace_to_seconds(pace_str):
+    """Convert 'MM:SS' string to total seconds."""
+    try:
+        minutes, seconds = map(int, pace_str.split(":"))
+        return minutes * 60 + seconds
+    except:
+        return None  # if invalid format
+
+
 def load_and_preprocess(path):
     df = pd.read_csv(path)
 
-    features = ['Distance', 'Steps', 'Calories', 'Total Ascent', 'Total Descent']
-    target = 'Avg Pace'
-        # Keep only the columns we need
+    features = ["Distance", "Steps", "Calories", "Total Ascent", "Total Descent"]
+    target = "Avg Pace"
+    # Keep only the columns we need
     df = df[features + [target]]
 
-    # Convert all selected columns to numeric (invalid values become NaN)
-    for col in features + [target]:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
-
+    df["Steps"] = df["Steps"].astype(str).str.replace(",", "")
+    df["Avg Pace"] = df["Avg Pace"].apply(convert_pace_to_seconds)
     
+    # Convert all columns to numeric, coercing errors to NaN
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
 
+    df = df.dropna()
+    print(df.head())
     X = df[features].values
     y = df[target].values
 
@@ -24,3 +36,4 @@ def load_and_preprocess(path):
     X_scaled = scaler.fit_transform(X)
 
     return train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
